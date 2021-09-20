aee62ae9c00c4dffe01f5e15b99f07ee955b80b4
Pesinn <pesinn@gmail.com>
2021-09-20 15:47:38 +0200

    request factory added

--- content/factory/request.py
+++ content/factory/request.py
@@ -1,2 +1,14 @@
-def conv_req_to_dbreq():
-  return "bleh"
+def conv_req_to_search_obj(req):
+  search_obj = {}
+  if 'languages' in req.args:
+    search_obj["languages"] = req.args.get("languages")
+  if 'date_from' in req.args:
+    search_obj["date_from"] = req.args.get("date_from")
+  if 'date_to' in req.args:
+    search_obj["date_to"] = req.args.get("date_to")
+  if 'sources' in req.args:
+    search_obj["sources"] = req.args.get("sources")
+  if 'search' in req.args:
+    search_obj["search"] = req.args.get("search")
+  
+  return search_obj
