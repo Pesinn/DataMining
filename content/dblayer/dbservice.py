f5df2bf90ecc0be9ec41f94eef3499eeefd6c3b8
Pesinn <pesinn@gmail.com>
2021-09-20 22:20:09 +0200

    mongodb search

--- content/dblayer/dbservice.py
+++ content/dblayer/dbservice.py
@@ -1,4 +1,4 @@
 import content.dblayer.mongodb.mongo_service as mongodb
 
-def get_articles():
-  return mongodb.get_articles()
+def get_articles(search):
+  return mongodb.get_articles(search)
--- content/dblayer/mongodb/mongo_service.py
+++ content/dblayer/mongodb/mongo_service.py
@@ -18,33 +18,24 @@ def load_json():
   file.close()
   return data
 
-def get_articles():
+def get_articles(search):  
   if(config.get_db_collection() == "None"):
     return [x for x in load_json() if x['language'] == 'French' or x['language'] == 'English']
   else:
     filter = {"_id": 0}
-    return [x for x in _mydb[ARTICLES].find({
-    "$or" : 
-    [
-      {
-        "language" : { "$eq" : "English"}
-      },
-      {
-        "language" : { "$eq" : "Spanish"}
-      }
-    ]
-    }, filter)]
+    return [x for x in _mydb[ARTICLES].find(convert_search_obj_to_dbreq(search), filter)]
 
+# https://www.analyticsvidhya.com/blog/2020/08/query-a-mongodb-database-using-pymongo/
+def convert_search_obj_to_dbreq(search):
+  dbreq = {}
 
-def convert_search_obj_to_dbreq():
-  return ""
-    
-def create_filter():
-  x = {
-    "language": ["English", "Spanish"],
-    "dateFrom": "2020-01-01",
-    "dateTo": "2020-02-02",
-    "sources": ["BBC", "CNN"],
-    "search": ["Covid-19", "Alibaba Inc."]
-  }
-  return x
+  for i in search:
+    if(i == "languages"):
+      dbreq["language"] = { "$in" : search[i] }
+    if(i == "sources"):
+      dbreq["source"] = { "$in" : search[i] }
+    if(i == "date_from"):
+      dbreq["publish_date"] = { "$gt": search[i] }
+    if(i == "date_to"):
+      dbreq["publish_date"] = { "$lt": search[i] }
+  return {"$or": [dbreq]}
