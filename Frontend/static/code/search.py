feaeaf47cadd675f5a6229282cd97fd751bc4b2e
Pesinn <pesinn@gmail.com>
2021-09-23 11:16:08 +0200

    URL changed to backend request

--- Frontend/static/code/search.py
+++ Frontend/static/code/search.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def get_news_data(req):
-  return requests.request("GET", f'http://192.168.8.105:8080/api/v1/news_data?{req}').json()
+  return requests.request("GET", f'http://10.94.63.91:8080/api/v1/news_data?{req}').json()
   
 def get_news_data_from_file():
   file = open('static/test_data/articles.json',)
