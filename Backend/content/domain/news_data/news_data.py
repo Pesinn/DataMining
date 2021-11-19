6da85f081ea140e7c7ecfd933cb7d74ba3e6cba4
Pesinn <pesinn@gmail.com>
2021-11-19 09:21:50 +0100

    B: Cleanup in news_data domain

--- Backend/content/domain/news_data/news_data.py
+++ Backend/content/domain/news_data/news_data.py
@@ -1,6 +1,6 @@
 import content.dblayer.dbservice as dbservice
 import content.domain.sentiment.sentiment_analysis as sentiment
-import content.domain.sentiment.sentiment_analysis_factory as factory
+import content.domain.sentiment.sentiment_analysis_factory as sentiment_factory
 
 def get_news_data(search):
   return_arr = []
@@ -12,14 +12,13 @@ def get_news_data(search):
       article = {
         "title": d["title"]["text"],
         "description": d["description"]["text"],
-        "language": d["language"],
+        "language": d["article_language"],
         "source": d["source"],
-        "publish_date": d["publish_date"]
+        "publish_date": d["publish_date"],
       }
       articles.append(article)
-    s_analysis.append(factory.convert_db_data_to_domain_data_obj(data, i["search"][0]))
     final_obj = {
-      "sentiment_analysis": s_analysis[0]["sentiment_analysis"],
+      "sentiment_analysis": sentiment_factory.convert_db_data_to_domain_data_obj(data, i["search"][0])["sentiment_analysis"],
       "articles": articles,
       "search": i["search"][0]
     }
