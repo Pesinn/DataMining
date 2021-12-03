09e4eb792896352b01c550dad68e7ab10a6b1126
Pesinn <pesinn@gmail.com>
2021-12-03 17:04:01 +0100

    F: Search bar on entity choosen fixed

--- Frontend/static/utils/misc/search_bar.js
+++ Frontend/static/utils/misc/search_bar.js
@@ -2,8 +2,61 @@
 // The id of the search bar is "main-search-input"
 // ============================================
 
+var search_id = "main-search-input";
+
+function remove_from_search_bar(str) {
+  var searchString = document.getElementById(search_id).value;
+  if(str && str.length > 0) {
+    var search_array = splitSearchToArray(searchString, str)
+    var searchString = searchArrayToSearchString(search_array)
+    document.getElementById(search_id).value = searchString;
+  }
+}
+
+function searchArrayToSearchString(searchArray) {
+  searchString = ""
+  for (let i = 0; i < searchArray.length; i++) {
+    for (let a = 0; a < searchArray[i].length; a++) {
+      searchString = searchString + ", " + searchArray[i][a];
+    }
+
+    // To make sure we don't add "|" in last loop
+    if(i < searchArray.length-1) {
+      searchString = searchString + "|";
+    }
+  }
+
+  // Make sure there are no trailing commas
+  return searchString.replace(/(^[,\s]+)|([,\s]+$)/g, '');
+}
+
+function splitSearchToArray(searchString, str) {
+  var final_search = []
+
+  if(str && str.length > 0) {
+    //var searchValue = document.getElementById(search_id).value;
+    var groups = searchString.split("|");
+
+    for (let i = 0; i < groups.length; i++) {
+      var searchTerms = groups[i].split(",")
+      var singleSearch = [];
+
+      for (let a = 0; a < searchTerms.length; a++) {
+        trimmedTerm = searchTerms[a].trim();
+        
+        if(trimmedTerm !== str && trimmedTerm.length > 0) {
+          singleSearch.push(searchTerms[a].trim())
+        }
+      }
+
+      final_search.push(singleSearch);
+    }
+  }
+  return final_search;
+}
+
 function append_to_search_bar(str) {
-  var searchValue = document.getElementById("main-search-input").value
+  var searchValue = document.getElementById(search_id).value
 
   if(!searchValue) {
     searchValue = searchValue + str    
@@ -18,6 +71,6 @@ function append_to_search_bar(str) {
     }
   }
 
-  document.getElementById("main-search-input").value = searchValue;
+  document.getElementById(search_id).value = searchValue;
 }
 
