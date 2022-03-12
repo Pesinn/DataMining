864749aa686eae9ab3020baf0b4756fcac5a3c7b
Pesinn <pesinn@gmail.com>
2022-03-12 15:52:12 +0100

    F: Code moved to sentiment_factory.py

--- Frontend/static/code/domain/sentiment/sentiment.py
+++ Frontend/static/code/domain/sentiment/sentiment.py
@@ -1,11 +1,11 @@
 import requests
 import static.code.domain.news_data.news_data as news_data
+import static.code.domain.sentiment.sentiment_factory as sent_fact
 import config
-import numpy as nu
 
 def get_sentiment_analysis(req):
   data = get_sentiment_analysis_service(req)
-  data = normalize_sentiment_analysis_arr(data)
+  data = sent_fact.normalize_sentiment_analysis_arr(data)
   return data
 
 def get_sentiment_analysis_file():
@@ -13,14 +13,3 @@ def get_sentiment_analysis_file():
 
 def get_sentiment_analysis_service(req):
   return requests.request("GET", f'{config.BACKEND_URL}/api/v1/sentiment?{req}').json()
-
-def normalize_sentiment_analysis_arr(data):
-  for i in data:
-    i = normalize_sentiment_analysis_obj(i)
-  return data
-
-def normalize_sentiment_analysis_obj(data):
-  total = float(data["sentiment_analysis"]["compound"]["all"]["freq"])
-  for i in data["sentiment_analysis"]["compound"]:
-    data["sentiment_analysis"]["compound"][i]["norm"] = nu.round((float(data["sentiment_analysis"]["compound"][i]["freq"]) / total) * 100)
-  return data
---
+++ Frontend/static/code/domain/sentiment/sentiment_factory.py
@@ -0,0 +1,12 @@
+import numpy as nu
+
+def normalize_sentiment_analysis_arr(data):
+  for i in data:
+    i = normalize_sentiment_analysis_obj(i)
+  return data
+
+def normalize_sentiment_analysis_obj(data):
+  total = float(data["sentiment_analysis"]["compound"]["all"]["freq"])
+  for i in data["sentiment_analysis"]["compound"]:
+    data["sentiment_analysis"]["compound"][i]["norm"] = nu.round((float(data["sentiment_analysis"]["compound"][i]["freq"]) / total) * 100)
+  return data
